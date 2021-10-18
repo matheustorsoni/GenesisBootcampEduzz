abbf3c4c19232e2b3291d81367c27e9898d3399a
matheustorsoni <matheus_tcampos@hotmail.com>
2021-10-18 18:58:33 -0300

    variaveis adicionadas

---
+++ script.js
@@ -0,0 +1,14 @@
+let order = [];
+let clickedOrder = [];
+let score = 0;
+
+// 0 = verde
+// 1 = vermelho
+// 2 = amarelo
+// 3 = azul
+
+const blue = document.querySelector ('.blue');
+const blue = document.querySelector ('.red');
+const blue = document.querySelector ('.green');
+const blue = document.querySelector ('.yellow');
+
